7387666234eb55f8f15192c1c6eaf660818941f0
liz-ish <30779260+liz-ish@users.noreply.github.com>
2020-03-08 19:52:46 -0600

    Update returnmyobject.js

--- returnmyobject.js
+++ returnmyobject.js
@@ -1,9 +1,1 @@
-<script>
-var myobject={
-  "success": true, 
-  "score": 0.5,
-  "action": "submit",
-
-}
-console.log("this is from JS file");
-</script>
+{ "success": "true", "score": "0.5","action": "submit"}
